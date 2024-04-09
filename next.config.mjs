fef3be14c67b78b36c2c7b6140c14276d1443de8
ttvu <thanhvu6a1@gmail.com>
2024-04-09 10:31:02 +0700

    fix: package json

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,7 @@ const nextConfig = {
   images: {
     domains: [
       "localhost",
+      "mytwt-api.xyz",
       "png.pngtree.com",
       "mytweets-bucket.s3.ap-southeast-1.amazonaws.com",
     ],
