6027555c16f26cf0b783e10d3cc15c839c492553
ttvu <thanhvu6a1@gmail.com>
2024-04-24 00:10:46 +0700

    fix: .env

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,20 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
+    // customKey: "my-value",
+    // SERVER: "http://localhost:3000",
+    // JWT_SECRET_ACCESS_TOKEN: "@vu150802",
+    // JWT_SECRET_REFRESH_TOKEN: "@vu140923",
+    // NEXTAUTH_SECRET: "@vu070802",
+    // CLIENT: "http://localhost:3001",
+    // WORKER: "http://localhost:3002",
     customKey: "my-value",
-    SERVER: "http://localhost:3000",
+    SERVER: "https://notify-j4qr.onrender.com",
     JWT_SECRET_ACCESS_TOKEN: "@vu150802",
     JWT_SECRET_REFRESH_TOKEN: "@vu140923",
     NEXTAUTH_SECRET: "@vu070802",
-    CLIENT: "http://localhost:3001",
-    WORKER: "http://localhost:3002",
+    CLIENT: "https://www.mytweet.one",
+    WORKER: "https://mytwt-api.xyz",
   },
   images: {
     domains: [
